61d4682635573a41240f0d063d03b2800d8e714b
TinaZhong <tz102@wellesley.edu>
2024-09-18 21:37:53 -0400

    add check if user already logged in before logging in

--- server/concepts/sessioning.ts
+++ server/concepts/sessioning.ts
@@ -24,6 +24,11 @@ export default class SessioningConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    
+    // Check if a user is already logged in
+    if (session.user !== undefined) {
+      throw new Error("User is already logged in. Please log out first.");
+    }
     session.user = username;
   }
 
